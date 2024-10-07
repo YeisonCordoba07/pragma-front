9e0a2b0a7c85418730bf20cb38b215524629fabd
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-06 23:48:29 -0500

    Revert "feat: add icon button with styles"
    This reverts commit 4a07621338a54f2b0cfb572e5a8dbab18329ea15.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IconButtonComponent } from './atoms/icon-button/icon-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    IconButtonComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
