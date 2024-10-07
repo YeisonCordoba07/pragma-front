5a6461d9e455e694ccaf3f38e25fe7df28409752
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-07 08:40:25 -0500

    feat: add secondary icon button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconButtonComponent } from './atoms/icon-button/icon-button.component';
 import { InputTextComponent } from './atoms/input-text/input-text.component';
+import { SecondaryIconButtonComponent } from './atoms/secondary-icon-button/secondary-icon-button.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     IconButtonComponent,
-    InputTextComponent
+    InputTextComponent,
+    SecondaryIconButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/atoms/secondary-icon-button/secondary-icon-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondary-icon-button',
+  templateUrl: './secondary-icon-button.component.html',
+  styleUrls: ['./secondary-icon-button.component.scss']
+})
+export class SecondaryIconButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
