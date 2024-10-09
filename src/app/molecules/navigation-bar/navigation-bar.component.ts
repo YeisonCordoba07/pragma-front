d79957f769d431872e4d0db26c9f3927327a3e91
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-09 15:30:46 -0500

    feat: add button to close nav bar

--- src/app/molecules/navigation-bar/navigation-bar.component.ts
+++ src/app/molecules/navigation-bar/navigation-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationBarComponent implements OnInit {
 
+  @Output() closeNav: EventEmitter<any> = new EventEmitter();
   constructor() { }
 
   ngOnInit(): void {
