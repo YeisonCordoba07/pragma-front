3dc6e685665775e6e53570eaa422b7c322963120
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-09 20:20:08 -0500

    refactor: remove unused  import

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import {HttpClient, HttpHeaders, HttpResponse} from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
