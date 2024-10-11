3cbc25e49d58197fb3410481a4056c2cba364669
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-10 22:46:16 -0500

    test: add test to header

--- src/app/molecules/header/header.component.spec.ts
+++ src/app/molecules/header/header.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
+import {By} from "@angular/platform-browser";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -19,5 +20,25 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should toggle "active" property when setActive is called', () => {
+    expect(component.active).toBe(false); // Estado inicial
+    component.setActive();
+    expect(component.active).toBe(true); // Estado después de la primera llamada
+    component.setActive();
+    expect(component.active).toBe(false); // Estado después de la segunda llamada
+  });
+
+  it('should toggle the "header-element__active" class on app-navigation-bar', () => {
+    let navigationBar = fixture.debugElement.query(By.css('app-navigation-bar'));
+    expect(navigationBar.nativeElement.classList).not.toContain('header-element__active');
+
+    component.setActive();
+    fixture.detectChanges(); // Actualiza el DOM
+
+    navigationBar = fixture.debugElement.query(By.css('app-navigation-bar'));
+    expect(navigationBar.nativeElement.classList).toContain('header-element__active');
   });
 });
