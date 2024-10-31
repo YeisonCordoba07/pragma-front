d9f993b0316e14439ef5bb3f83ca9719de217221
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2024-10-30 20:50:55 -0500

    refactor: update token

--- src/app/constants/service.constants.ts
+++ src/app/constants/service.constants.ts
@@ -1,5 +1,4 @@
 export const TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhZG1pbkBleGFtcGxlLmNvbSIsInJvbGVzIjoiQURNSU4iLCJpYXQiOjE3MjkzOTM0MTEsImV4cCI6MTczMTk4NTQxMX0.cQDOqMKqfvsfGdxsI74CJLdbHrCG_xTDkat9uNWxbhk";
-
 export const CATEGORY_CREATION_URL = "http://localhost:8080/category";
 export const BRAND_CREATION_URL = "http://localhost:8080/brand";
 export const ITEM_CREATION_URL = "http://localhost:8080/item";
